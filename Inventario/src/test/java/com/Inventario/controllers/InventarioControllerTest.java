530222a664231d5bcb85e09855717b9ece3e0e09
Dante Schacht <dantesch10@gmail.com>
2025-07-01 20:46:45 -0400

    Se agrego la prueba con junit a controller

---
+++ Inventario/src/test/java/com/Inventario/controllers/InventarioControllerTest.java
@@ -0,0 +1,30 @@
+package com.Inventario.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class InventarioControllerTest {
+    @Test
+    void testActualizar() {
+
+    }
+
+    @Test
+    void testCrear() {
+
+    }
+
+    @Test
+    void testEliminar() {
+
+    }
+
+    @Test
+    void testListar() {
+
+    }
+
+    @Test
+    void testObtener() {
+
+    }
+}
